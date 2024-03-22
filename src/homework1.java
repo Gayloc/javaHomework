5eb5c7dde766ee257bd2268932dc540fca6098e8
Gayloc <gu18647403665@outlook.com>
2024-03-22 11:50:53 +0800

    第一次作业完成

---
+++ src/homework1.java
@@ -0,0 +1,14 @@
+public class homework1 {
+    public static void main(String[] args) {
+        System.out.println("创建一个对象");
+        HuZhengPeng hzp = new HuZhengPeng();
+    }
+}
+
+class HuZhengPeng {
+    HuZhengPeng() {
+        System.out.println(this+"说:");
+        System.out.println("我是胡振鹏，我要打印我的地址");
+        System.out.println(this.hashCode());
+    }
+}
